cd0664e44a8e1309f87a086dd34c5aca220b2789
ocwebkings <kevin@ocwebkings.com>
2019-09-06 12:57:45 -0700

    Completed exercise.

---
+++ array-literals/index.js
@@ -0,0 +1,16 @@
+var arrayOfStrings = ["String 1", "String 2"];
+var arrayOfNumbers = [1, 2, 3, 4];
+var arrayOfObjects = [
+    { name: "Object 1" },
+    { name: "Object 2" },
+]
+var arrayOfArrays = [
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4]
+]
+
+console.log(arrayOfStrings[0]);
+console.log(arrayOfNumbers[0]);
+console.log(arrayOfObjects[0].name);
+console.log(arrayOfArrays[0][0]);
