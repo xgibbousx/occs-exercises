1781f9ac79e01dad340c8fadab8fefad1316078c
ocwebkings <kevin@ocwebkings.com>
2019-09-06 12:49:09 -0700

    Completed exercise.

---
+++ object-literals/index.js
@@ -0,0 +1,12 @@
+var myFavoriteCar = {
+    make: "Ferrari",
+    model: "Spider",
+    year: 2019,
+    salePrice: 234456.00
+}
+
+console.log(myFavoriteCar);
+console.log(myFavoriteCar.make);
+console.log(myFavoriteCar.model);
+console.log(myFavoriteCar.year);
+console.log(myFavoriteCar.salePrice);
