e6c085651062ab0a490c9e9049f36bedb048e25e
ocwebkings <kevin@ocwebkings.com>
2019-09-06 11:47:35 -0700

    Completed exercise.

---
+++ string-concatenation/index.js
@@ -0,0 +1,9 @@
+var firstName = "Kevin";
+var lastName = "Bikhazi";
+var fullName = firstName + " " + lastName;
+var favoriteRestaurantAddress = "Favorite Restaurant\n" + 
+                                "Street\n" +
+                                "City/State/Zipcde";
+
+console.log(fullName);
+console.log(favoriteRestaurantAddress);
