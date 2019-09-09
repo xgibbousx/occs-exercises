20d3c9177cb08351733d8c42e6e71eee4fa015bc
ocwebkings <kevin@ocwebkings.com>
2019-09-09 14:32:36 -0700

    Completed exercise.

---
+++ while-loops/index.js
@@ -0,0 +1,10 @@
+function repeat(word, times) {
+    timesCompleted = 0;
+    while(timesCompleted < times) {
+        console.log(word);
+        timesCompleted++;
+    }
+}
+
+repeat("Kevin", 3);
+repeat ("John", 5);
